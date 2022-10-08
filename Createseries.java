716bccd0f8342480b863d77fe576b7715142e874
Vijeth Kumar <71746520+Vijethkumar24@users.noreply.github.com>
2022-10-08 18:49:21 +0530

    Add files via upload

---
+++ Createseries.java
@@ -0,0 +1,25 @@
+
+import java.util.*;
+import java.io.*;
+
+class Solution{
+    public static void main(String []argh){
+        Scanner in = new Scanner(System.in);
+        int t=in.nextInt();
+         int a,b,n;
+        for(int i=0;i<t;i++){
+             a = in.nextInt();
+             b = in.nextInt();
+             n = in.nextInt();
+        int sum=a;
+        for(int j=0;j<n;j++)
+        {
+            sum+=(Math.pow(2,j)*b);
+            System.out.print(sum+" ");
+        }
+        System.out.println();
+        }
+        in.close();
+      
+    }
+}
